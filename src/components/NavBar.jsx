24b0c11b80416f5cb49c5d6efd8737e471e46775
Bogdan <89359664+BoggoC@users.noreply.github.com>
2024-04-02 21:27:07 +0200

    add NavBar component plus css styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,47 @@ const NavBar = () => {
     );
   }
 
-  return <h1>NavBar</h1>;
+  return (
+    <nav className="nav-bar">
+      <div className="wrapper">
+        {data.map((navBar) => {
+          const { heroTitle, socialLink, about } = navBar;
+          return (
+            <nav className="links">
+              <a href="" className="title">
+                {heroTitle}
+              </a>
+              <div className="socials">
+                {socialLink.map((socials) => {
+                  const { socialId, socialTitle, socialImg, socialUrl } =
+                    socials;
+                  return (
+                    <div key={socialId} className="social-btn-container">
+                      <a
+                        href={socialUrl}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="social"
+                      >
+                        <img
+                          src={socialImg}
+                          alt={socialTitle}
+                          className="btn-container"
+                        />
+                      </a>
+                    </div>
+                  );
+                })}
+              </div>
+              <a href="" className="title">
+                {about}
+              </a>
+            </nav>
+          );
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
